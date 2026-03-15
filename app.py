a885b7d229d31bee14beec70d821a319b6fe0442
Varsha V Nayak <varshavnayak2003@gmail.com>
2026-03-15 15:26:53 +0530

    Updated streamlit app

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import streamlit as st
 from utils.resume_parser import extract_text_from_pdf
+from utils.skill_extractor import extract_skills
 
 st.title("AI Resume Analyzer")
 
 uploaded_file = st.file_uploader("Upload your resume (PDF)", type=["pdf"])
 
 if uploaded_file is not None:
+
     resume_text = extract_text_from_pdf(uploaded_file)
 
     st.subheader("Extracted Resume Text")
     st.write(resume_text)
+
+    skills = extract_skills(resume_text)
+
+    st.subheader("Detected Skills")
+
+    for skill in skills:
+        st.write("✔", skill)
