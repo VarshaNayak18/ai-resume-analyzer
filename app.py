e3641e53738c41a47bf95d41f062dc1ffaf2adb2
Varsha V Nayak <varshavnayak2003@gmail.com>
2026-03-15 18:10:13 +0530

    Added resume-job similarity score using NLP

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from utils.resume_parser import extract_text_from_pdf
 from utils.skill_extractor import extract_skills
+from utils.similarity import calculate_similarity
 
 st.title("AI Resume Analyzer")
 
 uploaded_file = st.file_uploader("Upload your resume (PDF)", type=["pdf"])
+
+job_description = st.text_area("Paste Job Description")
 
 if uploaded_file is not None:
 
@@ -19,3 +22,11 @@ if uploaded_file is not None:
 
     for skill in skills:
         st.write("✔", skill)
+
+    if job_description:
+
+        score = calculate_similarity(resume_text, job_description)
+
+        st.subheader("Resume Match Score")
+
+        st.write(f"{score}% match with job description")
